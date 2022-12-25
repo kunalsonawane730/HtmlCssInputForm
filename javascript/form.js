1abbe96571d5504ccd14a10a399db261ac617877
Kunal <kunalsonawane730@gmail.com>
2022-12-26 01:28:23 +0530

    Add: creating basic input form with javascript for validate password with all given rules

--- javascript/form.js
+++ javascript/form.js
@@ -17,12 +17,12 @@ const text = document.querySelector('#text');
             });
 
     const email = document.querySelector('#email');
-    const emailError = document.querySelector('.email-error');
-    email.addEventListener('input', function () {
-        let emailRegex = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
-        if (emailRegex.test(email.value))
-            emailError.textContent = "";
-        else emailError.textContent = "Incorrect Email Address.";
+        const emailError = document.querySelector('.email-error');
+        email.addEventListener('input', function () {
+            let emailRegex = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
+            if (emailRegex.test(email.value))
+                emailError.textContent = "";
+            else emailError.textContent = "Incorrect Email Address.";
         });
 
     const telephone = document.querySelector('#tel');
@@ -35,14 +35,23 @@ const text = document.querySelector('#text');
             telephoneError.textContent = "Incorrect Contact Details.";
         });
 
-    const password = document.querySelector('#pwd');
-    const passwordError = document.querySelector('.pwd-error');
-    password.addEventListener('input', function() {
-        let passwordRegex = RegExp('^[a-zA-Z0-9]{8,}$')
-        if (passwordRegex.test(password.value)) {
-            passwordError.textContent = "";
-        } else {
-            passwordError.textContent = "Invalid Password";
-        }
-        });
+        /*
+        Rule1 – minimum 8 Characters
+        Rule2 – Should have at least 1
+        Upper Case
+        Rule3 – Should have at least 1
+        numeric number in the password
+        Rule4 – Has exactly 1 Special
+        Character
+        - NOTE – All rules must be passed
+        */
+        const password = document.querySelector('#pwd');
+        const passwordError = document.querySelector('.pwd-error');
+        password.addEventListener('input', function() {
+            let passwordRegex = RegExp('^(?=.*[A-z])(?=.*[0-9])(?=.*[@#$%^&*()-+=])([a-zA-Z0-9@._-]).{8,}$')
+            if (passwordRegex.test(password.value))
+                passwordError.textContent = "";
+             else
+                passwordError.textContent = "Invalid Password";
+            });
         
